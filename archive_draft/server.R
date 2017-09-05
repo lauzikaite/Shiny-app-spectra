e8296950c64013e7cac9f8b9a934b7c031f4366b
lauzikaite <elze.lauzikaite@gmail.com>
2017-09-05 12:50:42 +0100

    shiny

--- archive_draft/server.R
+++ archive_draft/server.R
@@ -3,19 +3,19 @@ library(ggplot2)
 library(shinythemes)
 
 # Load data ---------------------------------------------------------------
-static <- read.table("spectra_static.txt", header = T, stringsAsFactors = F)[,c("ppm","intensity5")]
+static <- read.table("/spectra_static.txt", header = T, stringsAsFactors = F)[,c("ppm","intensity5")]
 colnames(static) <- c("ppm", "intensity")
 
-reactive_1 <- read.table('Hippuric_acid_new.txt', header = T, stringsAsFactors = F)
+reactive_1 <- read.table('/Hippuric_acid_new.txt', header = T, stringsAsFactors = F)
 # default - almost none, intensity1 - huge levels, intensity2 - medium levels
 
-reactive_2 <- read.table('Tartaric_acid_new.txt', header = T, stringsAsFactors = F)
+reactive_2 <- read.table('/Tartaric_acid_new.txt', header = T, stringsAsFactors = F)
 # default - almost none, intensity1 - huge levels, intensity2 - medium levels
 
-reactive_3 <- read.table('L-Carnitine_new.txt', header = T, stringsAsFactors = F)
+reactive_3 <- read.table('/L-Carnitine_new.txt', header = T, stringsAsFactors = F)
 # default - almost none, intensity1 - huge levels, intensity2 - medium levels
 
-reactive_4 <- read.table('TMAO_new.txt', header = T, stringsAsFactors = F)
+reactive_4 <- read.table('/TMAO_new.txt', header = T, stringsAsFactors = F)
 # default - almost none, intensity1 - huge levels, intensity2 - medium levels
 
 # Server ------------------------------------------------------------------
